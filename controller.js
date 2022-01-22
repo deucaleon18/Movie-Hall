c8abc9ce9defb78772524d64b30fe9681b6b2acb
deucaleon18 <shubham37908@gmail.com>
2022-01-23 04:25:50 +0530

    grid-added

--- controller.js
+++ controller.js
@@ -1,34 +1,53 @@
 const path = require('path')
 const fs = require('fs');
-const data = ["Movie 1","Movie 2","Movie 3","Movie 4"];
+
+
+
+let data = JSON.parse(fs.readFileSync(
+    path.resolve(__dirname+"/Database/SeatMatrix.json")
+));
+
+
 const home = (req,res)=>{
     res.render('home',{title:'Home'});
+    
  }
+
+
  const getid = (req,res)=>{
-    const id=req.params.id -1;
+
+    const id=req.params.id-1;
+    console.log(id)
       res.render('seats',{index:id,data:data});
         
 }
+
+
  const err = (req,res)=>{
     res.render('error',{title:'Error'});
 }
+
+
 const bookslot = (req,res) => {
     const seats_booked = req.body.seats;
     const movie_name = req.params.id;
-    let data = JSON.parse(fs.readFileSync(
-        path.resolve(__dirname+"/Database/SeatMatrix.json")
-    ));
+
     let check = false;
+   
     seats_booked.forEach(element => {
-        if(data[movie_name][element-1]==1){
+        if(data[movie_name].seats[element-1]==1){
             check = true;
             res.sendStatus(400).json({"status":false});
         }
-        data[movie_name][element-1] = data[movie_name][element-1]^1; 
+        data[movie_name].seats[element-1] += 1; 
     });
+
     fs.writeFileSync(__dirname+"/Database/SeatMatrix.json",JSON.stringify(data));
     res.sendStatus(200);
 }
+
+
+
 const cancelslot = (req,res) => {
     const seats_cancel = req.body.seats;
     const movie_name = req.params.id;
@@ -36,9 +55,10 @@ const cancelslot = (req,res) => {
         path.resolve(__dirname+"/Database/SeatMatrix.json")
     ));
     seats_cancel.forEach(element => {
-        data[movie_name][element-1] = data[movie_name][element-1]^1; 
+        data[movie_name].seats[element-1] -= 1; 
     });
     fs.writeFileSync(__dirname+"/Database/SeatMatrix.json",JSON.stringify(data));
     res.sendStatus(200);
 }
+
 module.exports = {home,err,bookslot,cancelslot,getid}
--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const routes=express.Router();
 routes.get('/',controller.home);
 routes.post('/book/:id',controller.bookslot);
 routes.post('/cancel/:id',controller.cancelslot);
-routes.get('/:id',controller.getid);
+routes.get('/movie/:id',controller.getid);
 routes.use(controller.err);
 
 module.exports = routes;
