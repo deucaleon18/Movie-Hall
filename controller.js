55b7718fe767a32599a1a15676e67440056cb41a
srijan2002 <srjnmajumdar8@gmail.com>
2022-01-22 20:11:56 +0530

    Added booking view with specific id

--- controller.js
+++ controller.js
@@ -1,10 +1,17 @@
 
+const data = ["Movie 1","Movie 2","Movie 3","Movie 4"];
+
+
 const home = (req,res)=>{
     res.render('home',{title:'Home'});
  }
-
+ const getid = (req,res)=>{
+    const id=req.params.id -1;
+      res.render('seats',{index:id,data:data});
+        
+}
  const err = (req,res)=>{
-    res.send("ERROR");
+    res.render('error',{title:'Error'});
 }
 
- module.exports = {home,err}
+ module.exports = {home,err,getid}
--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const express=require('express');
 const routes=express.Router();
 
 routes.get('/',controller.home);
+routes.get('/:id',controller.getid);
 routes.use(controller.err);
 
 module.exports = routes;
